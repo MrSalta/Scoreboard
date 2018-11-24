6f7c0a80b396187fa8bcf4df27a6e56519cc36b8
Steve Mbouadeu <mbouadeus@gmail.com>
2018-11-24 08:44:19 -0500

    Updates

---
+++ precache-manifest.2101b4ab240450c23a7ed3c39da26cea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc1b41325083169089dd",
+    "url": "/scoreboard/static/js/runtime~main.dc1b4132.js"
+  },
+  {
+    "revision": "0c5e529dd9fdd4d26e24",
+    "url": "/scoreboard/static/js/main.0c5e529d.chunk.js"
+  },
+  {
+    "revision": "872633813eb3847f201b",
+    "url": "/scoreboard/static/js/1.87263381.chunk.js"
+  },
+  {
+    "revision": "0c5e529dd9fdd4d26e24",
+    "url": "/scoreboard/static/css/main.b96454e8.chunk.css"
+  },
+  {
+    "revision": "6387f293b891e71df6ef53f80ea50769",
+    "url": "/scoreboard/index.html"
+  }
+];
